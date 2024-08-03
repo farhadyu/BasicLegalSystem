76cd7a40d1ff640140e60505cb9e53fe712b6d51
Farhad Yusifov <30278611+farhadyu@users.noreply.github.com>
2024-08-03 12:28:29 -0400

    Add files via upload
    defining states of being that make the most sense

---
+++ being_states.py
@@ -0,0 +1,39 @@
+class Spirit():
+    wave = True
+
+class Physical(Spirit):
+    object = True
+
+class Body(Physical):
+    history = True
+
+
+class Intact(Body):
+    metabolism = True
+
+class Identity(Intact):
+    preservation  = True
+
+class Perceiver(Identity):
+    vision = True
+
+
+class Instinct(Perceiver):
+    memory = True
+
+class Animal(Instinct):
+    nature = True
+
+class Human(Animal):
+    belief = True
+
+
+class Adam(Human):
+    obligations = True
+
+class Self(Adam):
+    acceptance = True
+
+class Buddha(Self):
+    shadow = True
+
